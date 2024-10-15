7e865f82f87d7b3b23808b2e68c813a10c7f39a4
KammererLara <lara.kammerer@gmail.com>
2024-10-15 11:07:09 +0200

    docs: add javadoc to StringService and StringController

--- src/main/java/io/muehlbachler/bswe/StringController.java
+++ src/main/java/io/muehlbachler/bswe/StringController.java
@@ -5,11 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ *Controller used for Rest-API
+ */
 @RestController
 public class StringController {
+    /**
+     *used StringService for changing String to Uppercase-String
+     */
     @Autowired
     private StringService stringService = new StringService();
 
+    /**
+     * api endpoint for changing string to uppercase at "/upper"
+     * @param text wanted string to get returned in uppercase
+     * @return uppercase string associated with param string
+     */
     @GetMapping("/upper")
     String upper(@RequestParam(value = "string") final String text) {
         return stringService.toUpperCase(text);
--- src/main/java/io/muehlbachler/bswe/StringService.java
+++ src/main/java/io/muehlbachler/bswe/StringService.java
@@ -2,10 +2,16 @@ package io.muehlbachler.bswe;
 
 import org.springframework.stereotype.Service;
 
-// FIXME: add javadoc
+/**
+ * Service for changing String to Uppercase-String
+ */
 @Service
 public class StringService {
-    // FIXME: add javadoc
+    /**
+     * changes string to uppercase
+     * @param string wanted string to get returned in uppercase
+     * @return uppercase string associated with param string
+     */
     public String toUpperCase(final String string) {
         // FIXME: implement
         return string;
